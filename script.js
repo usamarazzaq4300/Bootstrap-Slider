2a543e152b698321590d9e99e4171c47d0677f0a
unknown <usamarazzaq4300@hotmail.com>
2021-03-15 02:32:16 +0500

    preparing Bootstrap Slider.

---
+++ script.js
@@ -0,0 +1,78 @@
+var i = 0 ;
+document.getElementById('img').src = 'slide1.jpg'
+function increment()
+{   
+    i = i + 1 ;
+    if(i % 4 == 0)
+    {
+        document.getElementById('img').src = 'slide1.jpg'
+    }
+    if (i % 4 == 1)
+    {
+        document.getElementById('img').src = 'slide2.jpg'
+    }
+    if(i % 4 == 2)
+    {
+        document.getElementById('img').src = 'slide3.jpg'
+    }
+    if(i % 4 == 3)
+    {
+        document.getElementById('img').src = 'slide4.jpg'
+    }
+    
+    if(i % 4 == -1 )
+    {
+        document.getElementById('img').src = 'slide2.jpg'
+        
+    }
+    if (i %4 == -2 )
+    {
+        document.getElementById('img').src = 'slide3.jpg'
+    }
+    if (i %4 == -3 )
+    {
+        document.getElementById('img').src = 'slide4.jpg'
+    }   
+}
+function decrement()
+{
+    i = i - 1 ;
+    
+    if(i%4 ==0)
+    {
+        document.getElementById('img').src = 'slide1.jpg'
+    }
+    if(i % 4 == 1 )
+    {
+        document.getElementById('img').src = 'slide2.jpg'
+        
+    }
+    if (i % 4 == 2 )
+    {
+        document.getElementById('img').src = 'slide3.jpg'
+    }
+    if(i % 4 == 3)
+    {
+        document.getElementById("img").src = "slide4.jpg"
+    }
+    if(i % 3 == -1 )
+    {
+        document.getElementById('img').src = 'slide3.jpg'
+        
+        
+    }
+    if (i %4 == -2 )
+    {
+        document.getElementById('img').src = 'slide2.jpg'
+    }
+    if (i % 4 == -2 )
+    {
+        document.getElementById('img').src = 'slide4.jpg'
+    }
+
+}
+// function button()
+// {
+//     document.getElementsById('but').style.border = 'none'
+//     document.getElementsById('but').style.backgroundColor = 'rgb(47,67,87)' 
+// }
